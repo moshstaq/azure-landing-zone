9c6d1f6c9023ad635276c2707b6973289cf1f602
Mosh <mosh_shood@hotmail.com>
2026-02-13 20:37:09 +0000

    Enable OIDC authentication for Terraform backend

--- landing-zones/app-dev/workloads/compute/vm/backend.tf
+++ landing-zones/app-dev/workloads/compute/vm/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "sttfstate7tcl"
     container_name       = "tfstate"
     key                  = "lz-app-dev-vm.tfstate"
+    use_oidc             = true
   }
 }
