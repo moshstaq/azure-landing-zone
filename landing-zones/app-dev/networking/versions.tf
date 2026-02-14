5c33fda8e8b29755816b2eea3b4f31c92d2112a9
Mosh <mosh_shood@hotmail.com>
2026-02-14 11:49:37 +0000

    feat: add multi-module CI/CD with matrix strategy
    - Add terraform-modules.json configuration
    - Add terraform-matrix-plan.yml for PR validation
    - Add terraform-matrix-apply.yml for main branch deployment
    - Add missing backend.tf files for all modules

--- landing-zones/app-dev/networking/versions.tf
+++ landing-zones/app-dev/networking/versions.tf
@@ -8,12 +8,6 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstate7tcl"
-    container_name       = "tfstate"
-    key                  = "landing-zone-app-dev-networking.tfstate"
-  }
 }
 
 provider "azurerm" {
--- landing-zones/app-dev/workloads/compute/aci/versions.tf
+++ landing-zones/app-dev/workloads/compute/aci/versions.tf
@@ -8,12 +8,6 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstate7tcl"
-    container_name       = "tfstate"
-    key                  = "lz-app-dev-aci.tfstate"
-  }
 }
 
 provider "azurerm" {
