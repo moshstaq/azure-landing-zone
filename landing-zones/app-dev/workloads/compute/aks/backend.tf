87b449f3e8e0405a9420abdd0929af5a53000c04
Mosh <mosh_shood@hotmail.com>
2026-02-19 19:35:35 +0000

    Add AKS cluster with system and workload node pools
    - AKS cluster: aks-app-dev with Azure CNI networking
    - System node pool: 1x Standard_B2s (AzureLinux)
    - Workload node pool: 1x Standard_B2s (AzureLinux)
    - Azure Network Policy enabled
    - OMS agent integrated with law-platform
    - Added to CI/CD module registry

---
+++ landing-zones/app-dev/workloads/compute/aks/backend.tf
@@ -0,0 +1,14 @@
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "sttfstate7tcl"
+    container_name       = "tfstate"
+    key                  = "lz-app-dev-aks.tfstate"
+  }
+}
+
+
+
+
+
---
+++ landing-zones/app-dev/workloads/compute/aks/outputs.tf
@@ -0,0 +1,24 @@
+output "cluster_id" {
+  description = "AKS cluster ID"
+  value       = azurerm_kubernetes_cluster.this.id
+}
+
+output "cluster_name" {
+  description = "AKS cluster name"
+  value       = azurerm_kubernetes_cluster.this.name
+}
+
+output "cluster_fqdn" {
+  description = "AKS cluster FQDN"
+  value       = azurerm_kubernetes_cluster.this.fqdn
+}
+
+output "kube_config_command" {
+  description = "Command to get kubeconfig"
+  value       = "az aks get-credentials --resource-group ${data.terraform_remote_state.networking.outputs.resource_group_name} --name ${azurerm_kubernetes_cluster.this.name}"
+}
+
+output "node_resource_group" {
+  description = "Resource group containing AKS nodes"
+  value       = azurerm_kubernetes_cluster.this.node_resource_group
+}
