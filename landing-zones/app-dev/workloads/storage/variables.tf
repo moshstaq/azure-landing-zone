4e8004334f8dbae3c50165378a7bb7ab7e81d24c
Mosh <mosh_shood@hotmail.com>
2026-02-28 12:51:41 +0000

    docs: work notes for weeks 18-20 monitoring, DR, backup, cost management

--- landing-zones/app-dev/workloads/storage/variables.tf
+++ landing-zones/app-dev/workloads/storage/variables.tf
@@ -1,7 +1,3 @@
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
 
 variable "resource_group_name" {
   description = "Resource group for storage account"
